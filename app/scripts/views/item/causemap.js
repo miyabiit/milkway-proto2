4b12b38370d3ba310f223d7f092d265e91d7140c
baker@BALLOONGA <yamada.masahiro@gmail.com>
2014-02-22 20:21:23 +0900

    view some markers and infowindow.

--- app/scripts/views/item/causemap.js
+++ app/scripts/views/item/causemap.js
@@ -22,21 +22,31 @@ function( Backbone, CausemapTmpl,Causemap) {
 		/* on render callback */
 		onRender: function() {
 			var cause = new Causemap();
-			var marker;
+			var markers = [];
 
 			var map = new google.maps.Map(this.el, cause.mapOptions);
 			//this.ui.mapContainer.html(this.el); //不要?
 
 			google.maps.event.addListener(map, 'click', clickMap);
-
+			
 			function clickMap(event){
-				if(marker){marker.setMap(null)};
-				marker = new google.maps.Marker({
-					position: event.latLng,
+				createMarker(event.latLng, map);
+			};
+
+			function createMarker(latLng, map){
+				var infoWindow = new google.maps.InfoWindow();
+				var marker = new google.maps.Marker({
+					position: latLng,
 					draggable: true,
 					map: map
 				});
-			}
+
+				google.maps.event.addListener(marker, 'click', function() {
+					var info = '緯度:' + Math.round(marker.getPosition().lat() * 100)/100 + ' 経度:' + Math.round(marker.getPosition().lng() * 100)/100;
+					infoWindow.setContent(info);
+					infoWindow.open(map, marker);
+				});
+			};
 		},
 	});
 });
